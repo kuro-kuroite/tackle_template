f5af6b5302249007836d360f8a210319cedfc7f3
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-06-18 08:29:13 +0000

    [run]: rubocop --auto-correct

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,3 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 I18n.default_locale = :ja
-I18n.available_locales = [:ja, :en]
+I18n.available_locales = %i[ja en]
