a5464de21173528689d32752588fa5f8f57a0ee9
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-06-18 09:39:39 +0000

    [refactor]: sort routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'blogs#index'
 
   resources :blogs
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  root 'blogs#index'
 end
